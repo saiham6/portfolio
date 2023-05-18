2348be651612ccef6bffb475a92a573894038385
saiham6 <saiham.unknown@gmail.com>
2023-05-18 22:10:40 +0100

    Linting

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
           {children}
           <Footer />
         </ThemeProvider>
-        <Analytics />
+        <Analytics/>
       </body>
     </html>
   )
